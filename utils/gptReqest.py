1ebf00de5fbb965277654a0aa03d23c7121fe9fd
AdamKaniasty <adam.kaniasty@gmail.com>
2023-12-10 14:58:17 +0100

    keys

--- utils/gptReqest.py
+++ utils/gptReqest.py
@@ -1,10 +1,13 @@
 from openai import OpenAI
 import os
+import dotenv
+
+dotenv.load_dotenv(dotenv.find_dotenv("../.env"))
 
 
 def gptRequest(user, system, model='gpt-4', temperature=0.7):
     client = OpenAI(
-        api_key="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"
+        api_key=os.getenv("OPENAI_API_KEY"),
     )
     response = client.chat.completions.create(
         messages=[
